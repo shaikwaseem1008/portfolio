31c504089fc7ff720d6ccc3670d08f585bfd3bea
shaikwaseem1008 <140194457+shaikwaseem1008@users.noreply.github.com>
2023-07-21 22:35:20 +0530

    Add files via upload

---
+++ web.js
@@ -0,0 +1,8 @@
+
+let changingName = document.getElementById("changingName");
+
+
+let veiwBtn = document.getElementById("veiwBtn");
+veiwBtn.addEventListener("click",function(){
+    changingName.textContent = "Hi Umesh";
+})
